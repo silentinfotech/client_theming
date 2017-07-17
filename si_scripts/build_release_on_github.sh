94e11e3041c4ce1112fe8439f29d0e6f730c6f4c
Kashyap Prajapati <kashyap2690@gmail.com>
2017-07-17 18:07:43 +0530

    build script upload file renamed.

--- si_scripts/build_release_on_github.sh
+++ si_scripts/build_release_on_github.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ..
-git pull origin griddrive
+git pull origin griddrive_win
 docker run -v "$PWD:/home/user/" nextcloud-client-win32:v1 /home/user/win/build.sh $(id -u)
 VER=`git rev-parse --short=6 HEAD`
-export GITHUB_TOKEN=76add5b726663f8e4304399bd255587da82b9212
+export GITHUB_TOKEN=1d9b5aabe4041b35cb6b0631e51a3ddebeedd833
 github-release delete --user silentinfotech --repo client_theming --tag griddrive
 github-release release --user silentinfotech --repo client_theming --tag griddrive --name "Grid Drive client for windows." --description "Windows Build" --pre-release
-github-release upload --user silentinfotech --repo client_theming --tag griddrive --name "client-win32-${VER}.zip" --file build-win32/Nextcloud-2.3.2.1-setup.exe
+github-release upload --user silentinfotech --repo client_theming --tag griddrive --name "Griddrive-setup-win32-${VER}.exe" --file build-win32/Griddrive-2.3.2.1-setup.exe
